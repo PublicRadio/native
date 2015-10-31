647b937debba2ab18a8daea75f29122417260170
jabher <crbrus@gmail.com>
2015-10-31 16:51:53 +0300

    feat: working player component with shuffle

--- android/app/src/main/java/com/publicradionative/BackgroundPlayer.java
+++ android/app/src/main/java/com/publicradionative/BackgroundPlayer.java
@@ -1,17 +1,31 @@
 package com.publicradionative;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.util.Log;
 
+import java.util.Random;
+import java.util.ArrayList;
+
+import javax.annotation.Nullable;
+
 public class BackgroundPlayer extends ReactContextBaseJavaModule {
-    MediaPlayer mediaPlayer;
-    Context context;
+    MediaPlayer mediaPlayer = new MediaPlayer();
+    ReactContext context;
+    ArrayList<Track> tracks = new ArrayList<>();
+    Random random = new Random();
+
     private static final String TAG = "BackgroundPlayer";
 
     public BackgroundPlayer(ReactApplicationContext reactContext) {
@@ -19,24 +33,66 @@ public class BackgroundPlayer extends ReactContextBaseJavaModule {
         context = reactContext;
     }
 
+
+    private void sendEvent(ReactContext reactContext,
+                           String eventName,
+                           @Nullable WritableMap params) {
+        context
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+
     @Override
-    public String getName() {
-        return "BackgroundPlayer";
+    public String getName() { return "BackgroundPlayer"; }
+
+    private int getRandomTrackIndex() { return random.nextInt(tracks.size()); }
+
+    private Track getRandomTrack() { return tracks.get(getRandomTrackIndex()); }
+
+    private void playNextTrack() { playTrack(getRandomTrack()); }
+
+    public void playTrack(Track track) {
+        Log.i(TAG, "now playing: " + track.artist + " " + track.title);
+        //todo
+        WritableMap map = Arguments.createMap();
+        map.putString("artist", track.artist);
+        map.putString("title", track.title);
+        sendEvent(context, "PlayerTrackChange", map);
+        mediaPlayer.stop();
+        mediaPlayer = MediaPlayer.create(context, track.uri);
+        play();
+    }
+
+    @ReactMethod
+    public void setTrackList(ReadableArray items) {
+        tracks = new ArrayList<Track>();
+        int size = items.size();
+        for (int i = 0; i < size; i++)
+            tracks.add(new Track(items.getMap(i)));
     }
 
     @ReactMethod
     public void play() {
-        try {
-            Uri uri = Uri.parse("https://cs1-19v4.vk-cdn.net/p21/c2524b81b7eca9.mp3?extra=MaDGuMXXGmsIFHJR9mzPeqj4e6P8ZRwDKI8VFTtuLOetaPRDxm5Cwz2D1V_pSX_93Zi3YlKKuTdsDS5wpl6vPBqLRIO2Odk");
-            mediaPlayer = MediaPlayer.create(context, uri);
-            mediaPlayer.start();
-        } catch (Exception e) {
-            Log.wtf(TAG, "react" + e.getMessage());
-        }
+        try { mediaPlayer.start(); } catch (Exception e) { Log.wtf(TAG, "react" + e.getMessage()); }
     }
 
     @ReactMethod
-    public void pause() {
-        mediaPlayer.stop();
+    public void pause() { mediaPlayer.stop(); }
+
+    @ReactMethod
+    public void skip() { playNextTrack(); }
+}
+
+
+class Track {
+    Uri uri;
+    String artist;
+    String title;
+
+    public Track(ReadableMap trackInfo) {
+        uri = Uri.parse(trackInfo.getString("uri"));
+        artist = trackInfo.getString("artist");
+        title = trackInfo.getString("title");
     }
 }
