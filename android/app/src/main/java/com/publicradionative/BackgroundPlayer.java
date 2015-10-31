6f6844f3e4e2bc5828441fa6ed14009c2c3295a6
jabher <crbrus@gmail.com>
2015-10-31 15:03:12 +0300

    feat: working player component

--- android/app/src/main/java/com/publicradionative/BackgroundPlayer.java
+++ android/app/src/main/java/com/publicradionative/BackgroundPlayer.java
@@ -4,17 +4,19 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 
-import java.io.File;
-
+import android.content.Context;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.util.Log;
 
 public class BackgroundPlayer extends ReactContextBaseJavaModule {
     MediaPlayer mediaPlayer;
+    Context context;
     private static final String TAG = "BackgroundPlayer";
 
     public BackgroundPlayer(ReactApplicationContext reactContext) {
         super(reactContext);
+        context = reactContext;
     }
 
     @Override
@@ -25,8 +27,8 @@ public class BackgroundPlayer extends ReactContextBaseJavaModule {
     @ReactMethod
     public void play() {
         try {
-            mediaPlayer = new MediaPlayer();
-            mediaPlayer.setDataSource("https://cs1-19v4.vk-cdn.net/p21/c2524b81b7eca9.mp3?extra=MaDGuMXXGmsIFHJR9mzPeqj4e6P8ZRwDKI8VFTtuLOetaPRDxm5Cwz2D1V_pSX_93Zi3YlKKuTdsDS5wpl6vPBqLRIO2Odk");
+            Uri uri = Uri.parse("https://cs1-19v4.vk-cdn.net/p21/c2524b81b7eca9.mp3?extra=MaDGuMXXGmsIFHJR9mzPeqj4e6P8ZRwDKI8VFTtuLOetaPRDxm5Cwz2D1V_pSX_93Zi3YlKKuTdsDS5wpl6vPBqLRIO2Odk");
+            mediaPlayer = MediaPlayer.create(context, uri);
             mediaPlayer.start();
         } catch (Exception e) {
             Log.wtf(TAG, "react" + e.getMessage());
