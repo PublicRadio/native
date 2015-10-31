7e070fd4556e2ae4f8642db1d521c9d7ef5de698
jabher <crbrus@gmail.com>
2015-10-31 12:49:36 +0300

    feat: icons

--- index.android.js
+++ index.android.js
@@ -4,49 +4,56 @@
  */
 'use strict';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+import React, {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 var PublicRadioNative = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render: function () {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Button!
+                </Text>
+                <Icon
+                    name='pause'
+                    size={300}
+                    color='#333333'
+                />
+                <TouchableHighlight onPress={() => {}}>
+                    <Icon
+                        name='pause'
+                        size={30}
+                        color='#333333'
+                    />
+                </TouchableHighlight>
+            </View>
+        );
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('PublicRadioNative', () => PublicRadioNative);
