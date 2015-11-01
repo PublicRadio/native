779b084d65cc800aec45d487c28ea9076c4bd2db
Sergey Lapin <lapanoid@gmail.com>
2015-11-01 06:06:24 +0300

    fix: delete deprecated MainActivity

--- android/app/src/main/java/com/publicradionative/MusicPlayerActivity.java
+++ android/app/src/main/java/com/publicradionative/MusicPlayerActivity.java
@@ -29,7 +29,6 @@ import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-
 import java.util.List;
 
 /**
@@ -38,7 +37,6 @@ import java.util.List;
 public class MusicPlayerActivity extends AppCompatActivity  implements DefaultHardwareBackBtnHandler {
     private ReactInstanceManager mReactInstanceManager;
     private ReactRootView mReactRootView;
-
     private MediaBrowser mMediaBrowser;
 
     private final MediaBrowser.ConnectionCallback mConnectionCallback =
