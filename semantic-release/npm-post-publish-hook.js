6a9c4b737ec92d6a1e4698819654b8fbb9e532b4
Sergey Lapin <lapanoid@gmail.com>
2015-11-12 02:51:03 +0300

    chore(ci):

---
+++ semantic-release/npm-post-publish-hook.js
@@ -0,0 +1,60 @@
+import SemanticReleaseError from 'semantic-release/error';
+import npmlog from 'npmlog';
+import RegClient from 'npm-registry-client';
+import publishPlaystore from 'publish-playstore';
+
+const npmClientGetPromise = (url, opt)=>
+  new Promise((resolve, reject)=>{
+    client.get(url, opt, (err, data) => {
+      if (err) {
+        reject(err);
+        return 
+      }
+      resolve(data);
+    }  
+  })
+
+export default (pluginConfig, {pkg, npm, plugins, options}, cb) => {
+  npmlog.level = npm.loglevel || 'warn'
+  let clientConfig = {log: npmlog}
+  // disable retries for tests
+  if (pluginConfig && pluginConfig.retry) clientConfig.retry = pluginConfig.retry
+  const client = new RegClient(clientConfig)
+
+  npmClientGetPromise(`${npm.registry}${pkg.name.replace('/', '%2F')}`, {auth: npm.auth})
+  .catch((err)=>{
+    if (err && (err.statusCode === 404 || /not found/i.test(err.message))) {
+      return cb(null, {})
+    }
+
+    return cb(err);
+  })
+  .then((data)=>{
+    let version = data['dist-tags'][npm.tag]
+
+    if (!version &&
+        options &&
+        options.fallbackTags &&
+        options.fallbackTags[npm.tag] &&
+        data['dist-tags'][options.fallbackTags[npm.tag]]) {
+      version = data['dist-tags'][options.fallbackTags[npm.tag]]
+    }
+
+    if (!version) {
+      return cb(new SemanticReleaseError(
+`There is no release with the dist-tag "${npm.tag}" yet.
+Tag a version manually or define "fallbackTags".`, 'ENODISTTAG'))
+    }
+    return version;
+  })
+  .then((version)=>publishPlaystore(version).then(()=>version))
+  .then((version)=>{
+    cb(null, {
+      version,
+      gitHead: data.versions[version].gitHead,
+      get tag () {
+        npmlog.warn('deprecated', 'tag will be removed with the next major release')
+        return npm.tag
+      }
+  })})
+}
