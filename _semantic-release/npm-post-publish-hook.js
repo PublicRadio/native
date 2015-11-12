f8f95e0e8bc1f9039492f0d17d9f3a4ab118f01d
Sergey Lapin <lapanoid@gmail.com>
2015-11-12 06:06:39 +0300

    fix(): test

--- _semantic-release/npm-post-publish-hook.js
+++ _semantic-release/npm-post-publish-hook.js
@@ -1,64 +1,13 @@
-const SemanticReleaseError = require('@semantic-release/error');
-const npmlog = require('npmlog');
-const RegClient = require('npm-registry-client');
-const publishPlaystore = require('./publish-playstore');
+const url = require('url')
 
-const npmClientGetPromise = (client)=>(url, opt)=>
-  new Promise((resolve, reject)=>{
-    client.get(url, opt, (err, data) => {
-      if (err) {
-        reject(err);
-        return 
-      }
-      resolve(data);
-    })  
-  })
+const parseSlug = require('parse-github-repo-url')
+const githubPost = require('./github-post')
 
-module.exports = function (pluginConfig, {pkg, npm, plugins, options}, cb)  {
-  npmlog.level = npm.loglevel || 'warn'
-  let clientConfig = {log: npmlog}
-  // disable retries for tests
-  if (pluginConfig && pluginConfig.retry) clientConfig.retry = pluginConfig.retry
-  const client = new RegClient(clientConfig)
+const beforeGitHub = (config, cb){
+  console.log(config)
+  cb()
+}
 
-  npmClientGetPromise(client)(`${npm.registry}${pkg.name.replace('/', '%2F')}`, {auth: npm.auth})
-  .catch((err)=>{
-    if (err && (err.statusCode === 404 || /not found/i.test(err.message))) {
-      return cb(null, {})
-    }
-
-    return cb(err);
-  })
-  .then((data)=>{
-    let version = data['dist-tags'][npm.tag]
-
-    if (!version &&
-        options &&
-        options.fallbackTags &&
-        options.fallbackTags[npm.tag] &&
-        data['dist-tags'][options.fallbackTags[npm.tag]]) {
-      version = data['dist-tags'][options.fallbackTags[npm.tag]]
-    } 
-
-    if (!version) {
-      return cb(new SemanticReleaseError(
-`There is no release with the dist-tag "${npm.tag}" yet.
-Tag a version manually or define "fallbackTags".`, 'ENODISTTAG'))
-    }
-    return {version, data};
-  })
-  .then(({version, data})=>{
-  	return publishPlaystore({version, data}).then(()=>({version, data}))
-  })
-  .then(({version, data})=>{
-  	console.log(data.versions[version].gitHead)
-    cb(null, {
-      version,
-      gitHead: data.versions[version].gitHead,
-      get tag () {
-        npmlog.warn('deprecated', 'tag will be removed with the next major release')
-        console.log(npm.tag)
-        return npm.tag
-      }
-  })})
+module.exports = function (config, cb) {
+  beforeGitHub(config, ()=>githubPost(config, cb))
 }
